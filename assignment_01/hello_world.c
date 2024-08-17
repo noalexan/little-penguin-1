2b882157cdf60d9fe2f07d7ab7ef6c66d701110a
noalexan <noalexan@student.42nice.fr>
2024-08-17 22:01:13 +0200

    assignment_01

---
+++ assignment_01/hello_world.c
@@ -0,0 +1,21 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Noah Alexandre");
+MODULE_DESCRIPTION("A hello world kernel module");
+MODULE_VERSION("1.0");
+
+static int __init hello_world_init(void)
+{
+    printk(KERN_INFO "Hello, World!\n");
+    return 0;
+}
+
+static void __exit hello_world_exit(void)
+{
+    printk(KERN_INFO "Cleaning up module.\n");
+}
+
+module_init(hello_world_init);
+module_exit(hello_world_exit);
