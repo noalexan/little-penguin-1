295c38471fde953eed252d28e7b4cc368f6e07c3
noalexan <noalexan@student.42nice.fr>
2024-09-02 19:01:21 +0200

    assignment_04

--- assignment_03/file.c
+++ assignment_03/file.c
@@ -1,31 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/delay.h>
 #include <linux/slab.h>
 
-int do_work(int *my_int, int return_value)
+MODULE_LICENSE("GPL");
+
+int do_work(int *my_int)
 {
 	int x;
 	int y = *my_int;
-	int z;
 
-	for (x = 0; x < my_int; ++x) {
+	for (x = 0; x < my_int; ++x)
 		udelay(10);
-	}
 
 	if (y < 10)
 		/* That was a long sleep, tell userspace about it */
 		pr_info("We slept a long time!");
 
-	z = x * y;
-	return z;
+	return x * y;
 }
 
 int my_init(void)
 {
 	int x = 10;
 
-	x = do_work(&x, x);
+	x = do_work(&x);
 	return x;
 }
 
